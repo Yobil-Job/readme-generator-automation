f531f2156ca29544463fa439212f9c28a1b20a63
Eyob Weldetensay <eyobwldetensay@gmail.com>
2025-05-25 16:00:48 +0300

    Update calculator.py

--- test-project/calculator.py
+++ test-project/calculator.py
@@ -7,6 +7,10 @@ def subtract(a, b):
     """Subtract b from a."""
     return a - b
 
+def subtract(a, b):
+    """Subtract b from a."""
+    return a - b
+
 def multiply(a, b):
     """Multiply two numbers."""
     return a * b
